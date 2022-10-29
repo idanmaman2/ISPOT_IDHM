c415ebf6080eaccf70a0533029a165da14866b0a
Idan Maman <90776557+idanmaman2@users.noreply.github.com>
2022-10-30 01:39:40 +0300

    new tracks page ---

--- ispot/lib/design/color.dart
+++ ispot/lib/design/color.dart
@@ -5,3 +5,16 @@ const Color spotifySecondry = Color.fromARGB(255, 1, 24, 64);
 const Color sliderThumbSpotiy = Colors.black54;
 const Color instagramMain = Colors.white;
 const Color instgramText = Colors.black;
+const  Map<int, Color> colorSpotifyMainPallet =
+{
+50:Color.fromRGBO(30,215,96, .1),
+100:Color.fromRGBO(30,215,96, .2),
+200:Color.fromRGBO(30,215,96, .3),
+300:Color.fromRGBO(30,215,96, .4),
+400:Color.fromRGBO(30,215,96, .5),
+500:Color.fromRGBO(30,215,96, .6),
+600:Color.fromRGBO(30,215,96, .7),
+700:Color.fromRGBO(30,215,96, .8),
+800:Color.fromRGBO(30,215,96, .9),
+900:Color.fromRGBO(30,215,96, 1),
+};
